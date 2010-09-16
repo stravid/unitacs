5fd48e0f5c702e6c39d5190c62779bd717865391
David Strauss <david.strausz@gmail.com>
2010-09-16 19:22:40 +0200

    add name handling

--- client/javascript/client.js
+++ client/javascript/client.js
@@ -6,14 +6,17 @@ var socket = new io.Socket(null, {port: 8080});
 
 socket.connect();
 socket.on('message', function(data) {
+    if (typeof(data) == "string") {
+        data = data.substr(3, data.length - 1);
+        data = JSON.parse(data);
+    }
     
     console.log('Master, you received praise: %o', data);
     
-    /*if (typeof(data) == "string") {
-        data = data.substr(3, data.length - 1);
-        data = JSON.parse(obj);
+    if (data.isNameTaken) {
+        document.getElementById('error').style.display = 'block';
     }
-    
+    /*
     if ('buffer' in data) {
         document.getElementById('form').style.display='block';
         document.getElementById('chat').innerHTML = '';
@@ -28,5 +31,5 @@ socket.on('message', function(data) {
 });
 
 document.getElementById('nameForm').onclick = function() {
-    socket.send({playerName: document.getElementById('playerName').value});  
+    socket.send({name: document.getElementById('playerName').value});  
 };
--- server/lib/unitacs.js
+++ server/lib/unitacs.js
@@ -4,12 +4,28 @@ function Unitacs(){
     sys.puts('Unitacs');
     
     this.takenNames = [];
+    this.games= [];
 };
 
 Unitacs.prototype.handleData = function(data, client) {
-    sys.puts(data);
+    sys.puts(sys.inspect(data));
     
     client.send(data);
+    
+    if (data.name) {
+        if (this.takenNames.contains(data.name)) {
+            client.send({isNameTaken: true});
+        } else {
+            client.name = data.name;
+            // IMPLEMENT: this.addPlayerToGame()
+        }
+    } else {
+        // IMPLEMENT: client.game.handleData(data, client)
+    }
+};
+
+Array.prototype.contains = function(item, from) {
+    return this.indexOf(item, from) != -1;
 };
 
 module.exports = Unitacs;
