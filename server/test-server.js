37e5f7dfebcf32c2ec4b9eb1910d3d0cce8b1446
David Strauss <david.strausz@gmail.com>
2010-10-10 17:41:55 +0200

    init test server

---
+++ server/test-server.js
@@ -0,0 +1,20 @@
+var http = require('http'), 
+    url = require('url'),
+    fs = require('fs'),
+    sys = require('sys'),
+    //io = require('../socket.io-server/lib/socket.io'),
+    Unitacs = require('./lib/unitacs');
+    
+    
+var unitacsServer = new Unitacs(),
+    DummyClient = function() {
+        
+    };
+
+DummyClient.prototype.send = function(message) {
+    
+};
+
+
+unitacsServer.handleData({name: 'stravid'}, new DummyClient());
+unitacsServer.handleData({name: 'ebsi'}, new DummyClient());
